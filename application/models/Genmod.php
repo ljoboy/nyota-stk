75d1c4bc768c065cf7beb6ac399f9c8cdcb0b6c7
Dark Angel <jonathanyombo@gmail.com>
2021-02-18 12:46:03 +0200

    [DEBUG] refactor and reformat code

--- application/models/Genmod.php
+++ application/models/Genmod.php
@@ -1,10 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class Genmod extends CI_Model{
-    
-    public function __construct() {
+class Genmod extends CI_Model
+{
+
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -25,20 +27,19 @@ class Genmod extends CI_Model{
      * @param mixed $whereColVal value of column $whereCol
      * @return boolean
      */
-    public function updateTableCol($tableName, $colName, $colVal, $whereCol, $whereColVal){
+    public function updateTableCol($tableName, $colName, $colVal, $whereCol, $whereColVal)
+    {
         $q = "UPDATE $tableName SET $colName = ? WHERE $whereCol = ?";
-        
+
         $this->db->query($q, [$colVal, $whereColVal]);
-        
-        if($this->db->affected_rows() > 0){
+
+        if ($this->db->affected_rows() > 0) {
             return TRUE;
-        }
-        
-        else{
+        } else {
             return FALSE;
         }
     }
-    
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -46,7 +47,7 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-    
+
     /**
      * get a single column from any table using a single column in the where clause
      * @param string $tableName
@@ -55,22 +56,21 @@ class Genmod extends CI_Model{
      * @param mixed $colValue
      * @return boolean
      */
-    public function getTableCol($tableName, $selColName, $whereColName, $colValue){
+    public function getTableCol($tableName, $selColName, $whereColName, $colValue)
+    {
         $q = "SELECT $selColName FROM $tableName WHERE $whereColName = ?";
-        
+
         $run_q = $this->db->query($q, [$colValue]);
-        
-        if($run_q->num_rows() > 0){
-            foreach($run_q->result() as $get){
+
+        if ($run_q->num_rows() > 0) {
+            foreach ($run_q->result() as $get) {
                 return $get->$selColName;
             }
-        }
-        
-        else{
+        } else {
             return FALSE;
         }
     }
-    
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -78,10 +78,10 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-    
-    
+
+
     /**
-     * 
+     *
      * @param type $event
      * @param type $eventRowIdOrRef
      * @param type $eventDesc
@@ -89,20 +89,19 @@ class Genmod extends CI_Model{
      * @param type $staffId
      * @return boolean
      */
-    public function addevent($event, $eventRowIdOrRef, $eventDesc, $eventTable, $staffId){
-        $data = ['event'=>$event, 'eventRowIdOrRef'=>$eventRowIdOrRef, 'eventDesc'=>$eventDesc, 'eventTable'=>$eventTable, 'staffInCharge'=>$staffId];
-        
+    public function addevent($event, $eventRowIdOrRef, $eventDesc, $eventTable, $staffId)
+    {
+        $data = ['event' => $event, 'eventRowIdOrRef' => $eventRowIdOrRef, 'eventDesc' => $eventDesc, 'eventTable' => $eventTable, 'staffInCharge' => $staffId];
+
         $this->db->insert('eventlog', $data);
-        
-        if($this->db->affected_rows() > 0){
+
+        if ($this->db->affected_rows() > 0) {
             return TRUE;
-        }
-        
-        else{
+        } else {
             return FALSE;
         }
     }
-    
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -110,28 +109,27 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-    
+
     /**
-     * 
+     *
      * @param type $admin_id
      * @return boolean
      */
-    public function get_admin_name($admin_id){
-       $q = "SELECT CONCAT_WS(' ', first_name, last_name) as 'name' FROM admin WHERE id = ?";
-       
-       $run_q = $this->db->query($q, [$admin_id]);
-       
-       if($run_q->num_rows() > 0){
-           foreach($run_q->result_array() as $get){
-               return $get['name'];
-           }
-       }
-       
-       else{
-           return FALSE;
-       }
-   }
-   
+    public function get_admin_name($admin_id)
+    {
+        $q = "SELECT CONCAT_WS(' ', first_name, last_name) as 'name' FROM admin WHERE id = ?";
+
+        $run_q = $this->db->query($q, [$admin_id]);
+
+        if ($run_q->num_rows() > 0) {
+            foreach ($run_q->result_array() as $get) {
+                return $get['name'];
+            }
+        } else {
+            return FALSE;
+        }
+    }
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -139,35 +137,30 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-   
-   /**
-    * 
-    * @param type $id
-    * @param type $table_name
-    * @return boolean
-    */
-   public function update_last_seen_time($id, $table_name){
+
+    /**
+     *
+     * @param type $id
+     * @param type $table_name
+     * @return boolean
+     */
+    public function update_last_seen_time($id, $table_name)
+    {
         //set the datetime based on the db driver in use
-        $this->db->platform() == "sqlite3" 
-                ? 
-        $this->db->set('last_seen', "datetime('now')", FALSE) 
-                : 
-        $this->db->set('last_seen', "NOW()", FALSE);
-        
+        $this->db->platform() == "sqlite3" ? $this->db->set('last_seen', "datetime('now')", FALSE) : $this->db->set('last_seen', "NOW()", FALSE);
+
         $this->db->where('id', $id);
 
         $this->db->update($table_name);
 
-        if($this->db->affected_rows()){
+        if ($this->db->affected_rows()) {
             return TRUE;
-        }
-
-        else{
+        } else {
             return FALSE;
         }
     }
-    
-    
+
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -175,38 +168,35 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-    
+
     /**
-     * 
+     *
      * @param type $year
      * @return boolean
      */
-    public function getYearEarnings($year=""){
+    public function getYearEarnings($year = "")
+    {
         $year_to_fetch = $year ? $year : date('Y');
-		
-        if($this->db->platform() == "sqlite3"){
-			$q = "SELECT transDate, totalPrice FROM transactions WHERE strftime('%Y', transDate) = '{$year_to_fetch}'";
-			
-			$run_q = $this->db->query($q);
-		}
-		
-		else{
-			$this->db->select('transDate, totalPrice');
-			$this->db->where(['YEAR(transDate)'=>$year_to_fetch]);
-			$run_q = $this->db->get('transactions');
-		}
-        
-        if($run_q->num_rows()){
-            return $run_q->result();
+
+        if ($this->db->platform() == "sqlite3") {
+            $q = "SELECT transDate, totalPrice FROM transactions WHERE strftime('%Y', transDate) = '{$year_to_fetch}'";
+
+            $run_q = $this->db->query($q);
+        } else {
+            $this->db->select('transDate, totalPrice');
+            $this->db->where(['YEAR(transDate)' => $year_to_fetch]);
+            $run_q = $this->db->get('transactions');
         }
-        
-        else{
+
+        if ($run_q->num_rows()) {
+            return $run_q->result();
+        } else {
             return FALSE;
         }
     }
-    
-    
-    
+
+
+
     /*
     ********************************************************************************************************************************
     ********************************************************************************************************************************
@@ -214,31 +204,29 @@ class Genmod extends CI_Model{
     ********************************************************************************************************************************
     ********************************************************************************************************************************
     */
-    
+
     /**
      * Get all mode of payments
      * @year
      * @return boolean
      */
-    public function getPaymentMethods($year){
-		if($this->db->platform() == "sqlite3"){
-			$q = "SELECT modeOfPayment FROM transactions WHERE strftime('%Y', transDate) GROUP BY ref";
-			
-			$run_q = $this->db->query($q);
-		}
-		
-		else{
-			$this->db->select('modeOfPayment');
-			$year ? $this->db->where('YEAR(transDate)', $year) : "";
-			$this->db->group_by('ref');
-			$run_q = $this->db->get('transactions');
-		}
-        
-        if($run_q->num_rows()){
-            return $run_q->result();
+    public function getPaymentMethods($year)
+    {
+        if ($this->db->platform() == "sqlite3") {
+            $q = "SELECT modeOfPayment FROM transactions WHERE strftime('%Y', transDate) GROUP BY ref";
+
+            $run_q = $this->db->query($q);
+        } else {
+            $this->db->select('modeOfPayment, COUNT(ref) as total');
+            $this->db->where('YEAR(transDate)', $year);
+            $this->db->group_by('modeOfPayment');
+            $run_q = $this->db->get('transactions');
         }
-        
-        else{
+
+
+        if ($run_q->num_rows()) {
+            return $run_q->result();
+        } else {
             return FALSE;
         }
     }
