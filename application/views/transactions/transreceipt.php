6d7e214d478e56ce6485fe051f0cb2d7043b9ed9
Roland Beni T <roland.beni1@gmail.com>
2021-05-06 23:55:22 +0200

    [UPDATE] define restau informations

--- application/views/transactions/transreceipt.php
+++ application/views/transactions/transreceipt.php
@@ -6,12 +6,12 @@ defined('BASEPATH') or exit('');
     <div id="transReceiptToPrint">
         <div class="row">
             <div class="col-xs-12 text-center text-uppercase">
-                <div style="margin-bottom:5px;"><img src="<?= base_url() ?>public/images/receipt_logo.png" alt="logo"
+            <div style="margin-bottom:5px;"><img src="<?= base_url() ?>public/images/nyota.png" alt="logo"
                                                      class="img-responsive center-block" width="60px"></div>
-                <h1>STK Management</h1>
-                <b>ID.NAT.: 441/84/EN/H.KAT 2018 <br/> RCCM: 17 A 0000 </b><br/>
-                <small>4, Av. Nyunzu Q/Roi Baudoin</small>
-                <div>Tél . : +243 000 000 000</div>
+                <h1>ETS NYOTA PLUS</h1>
+                <b>ID-NAT PROV: 441/128/EN/H-KAT/2020 <br/> RCCM: 17-A-5957</b><br/>
+                <small>05, Av. Cassitérite Q/MAKUTANO</small>
+                <div>Tél . : +243 820 230 699, +243 850 503 864, +243978664614</div>
             </div>
         </div>
         <hr style='margin-top:5px; margin-bottom:0'>
