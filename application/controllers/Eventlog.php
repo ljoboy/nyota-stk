d9c891a600e7e90820ffdaf76c308046f447f0df
Dark Angel <jonathanyombo@gmail.com>
2021-05-13 17:50:13 +0200

    aprovisionnement

--- application/controllers/Eventlog.php
+++ application/controllers/Eventlog.php
@@ -5,6 +5,8 @@ class Eventlog extends CI_Controller{
     
     public function __construct(){
         parent::__construct();
+
+        $this->load->model(['eventlog_Model']);
         
         $this->genlib->checkLogin();
         
@@ -13,9 +15,80 @@ class Eventlog extends CI_Controller{
     
     
     public function index(){
-        $data['pageContent'] = $this->load->view('eventlog', '', TRUE);
+        $data['logs'] = $this->eventlog_Model->getAll('id', 'DESC');
+        var_dump($data);die;
+
         $data['pageTitle'] = "Journal d'erreur";
-        
+
+        $data['pageContent'] = $this->load->view('eventlog', '', TRUE);
         $this->load->view('main', $data);
     }
+
+    public function approvisionnement()
+    {
+        $data['logs'] = $this->eventlog_Model->approvisionnement('id', 'DESC');
+        var_dump($data);die;
+
+        $data['pageTitle'] = "Journal d'erreur";
+
+        $data['pageContent'] = $this->load->view('eventlog', '', TRUE);
+        $this->load->view('main', $data);
+    }
+
+    /**
+     * "lilt" = "load Items List Table"
+     */
+    /*public function lilt()
+    {
+        $this->genlib->ajaxOnly();
+
+        $this->load->helper('text');
+
+        //set the sort order
+        $orderBy = $this->input->get('orderBy', true) ? $this->input->get('orderBy', true) : "name";
+        $orderFormat = $this->input->get('orderFormat', true) ? $this->input->get('orderFormat', true) : "DESC";
+
+        //count the total number of items in db
+        $totalItems = $this->db->count_all('items');
+
+        $this->load->library('pagination');
+
+        $pageNumber = $this->uri->segment(
+            3,
+            0
+        );//set page number to zero if the page number is not set in the third segment of uri
+
+        $limit = $this->input->get('limit', true) ? $this->input->get('limit', true) : 10;//show $limit per page
+        $start = $pageNumber == 0 ? 0 : ($pageNumber - 1) * $limit;//start from 0 if pageNumber is 0, else start from the next iteration
+
+        //call setPaginationConfig($totalRows, $urlToCall, $limit, $attributes) in genlib to configure pagination
+        $config = $this->genlib->setPaginationConfig(
+            $totalItems,
+            "items/lilt",
+            $limit,
+            ['onclick' => 'return lilt(this.href);']
+        );
+
+        $this->pagination->initialize($config);//initialize the library class
+
+        //get all items from db
+        $data['allItems'] = $this->item->getAll($orderBy, $orderFormat, $start, $limit);
+        $data['categories'] = $this->category_model->getAll();
+        $data['itemCategories'] = $this->category_model->getAllItemCategories();
+        $data['range'] = $totalItems > 0 ? "Afficher " . ($start + 1) . "-" . ($start + count(
+                    $data['allItems']
+                )) . " sur " . $totalItems : "";
+        $data['links'] = $this->pagination->create_links();//page links
+        $data['sn'] = $start + 1;
+        $data['cum_total'] = $this->item->getItemsCumTotal();
+        $data['critic_items'] = $this->item->getCriticItem();
+
+        $json['itemsListTable'] = $this->load->view(
+            'items/itemslisttable',
+            $data,
+            true
+        );//get view with populated items table
+
+        $this->output->set_content_type('application/json')->set_output(json_encode($json));
+    }*/
 }
